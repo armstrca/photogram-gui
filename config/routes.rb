ab0c03f411919bde59b8f138dd4ceb850c62c668
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-02 17:10:55 +0000

    user details

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
-  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:path_username", { :controller => "users", :action => "show" })
   # get("/comments")
   # get("/likes")
-  # get("/photos")
+  get("/photos")
   # get("follow_requests")
 end

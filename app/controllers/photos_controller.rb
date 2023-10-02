699fafc8509a93fc6f940c0bce20b1d80b4477c2
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-02 19:37:53 +0000

    more done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,26 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc })
     render( :template => "photo_templates/index")
   end
+
+  def show
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show" })
+  end
+
+  def destroy
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # get("/comments")
   # get("/likes")
   get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  get("/delete_photos/:path_id", { :controller => "photos", :action => "destroy" })
   # get("follow_requests")
 end

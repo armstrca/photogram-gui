b2f1bbde3a8df6ce8a8ca83fd5e55ca3ee08c857
Calvin Armstrong <138508650+armstrca@users.noreply.github.com>
2023-10-02 16:30:48 +0000

    users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
+  # get("/comments")
+  # get("/likes")
+  # get("/photos")
+  # get("follow_requests")
 end
